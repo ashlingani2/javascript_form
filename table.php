d7018d4f3e1af5cb1f50fdc0109c2c87464a21f6
ashlingani2 <ajbasilio23-0362@cca.edu.ph>
2025-02-19 18:42:42 +0800

    added a new file php
    hahah

---
+++ table.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Table Form</title>
+    </head>
+    <body>
+        
+        <h1>Student Record</h1>
+        <table id="student_record">
+            <!-- WALANG LAMAN -->
+        </table>
+
+
+    </body>
+</html>
